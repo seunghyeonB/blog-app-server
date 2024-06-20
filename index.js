d6b7eabce216533066fbf7eac5ebe6054bef2041
Seunghyeon Ban <127355005+seunghyeonB@users.noreply.github.com>
2024-06-19 21:34:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-app.use(cors({credentials: true, origin: 'https://66738354edca86c21f351a24--jocular-queijadas-801a0b.netlify.app'}))
+app.use(cors({credentials: true, origin: 'https://reliable-youtiao-16437f.netlify.app'}))
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
